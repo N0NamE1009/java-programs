9cd7eb7c9fdc8bc62f9652c0c69176f56de27d35
Luis Donaldo Barreto <luis.dbg1123@outlook.com>
2024-08-04 08:43:53 -0600

    Variables update

--- 1_introduction/Variables/src/Main.java
+++ 1_introduction/Variables/src/Main.java
@@ -12,7 +12,27 @@
 * */
 public class Main {
     public static void main(String[] args) {
-        int a=10;
-        System.out.println("Variable a: "+a);
+        //string
+        String name="Luis";
+        name="Donaldo";
+        String greeting;
+        greeting="Hi, my name is ";
+
+        //integer
+        int number=10;
+        number=number+5;
+
+        //boolean
+        boolean isGreater;
+        isGreater=false;
+
+        // real number
+        double real;
+        real=12.3;
+
+        System.out.println(greeting+name);
+        System.out.println("Variable number: "+number);
+        System.out.println("Boolean: "+isGreater);
+        System.out.println("Real number: "+real);
     }
 }
