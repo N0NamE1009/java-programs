f37a20f6f6444d04e45bea174bd8e4514db5a616
Luis Donaldo Barreto <luis.dbg1123@outlook.com>
2024-08-04 08:34:49 -0600

    Variables

---
+++ 1_introduction/Variables/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+* 1.- Can't have spaces
+* 2.- Can't start with a number
+* 3.- Cannot be a reserved word
+*
+* camelCase (most used)
+* snake_case
+* SCREAMING_SNAKE_CASE (constants)
+* kebab-case
+* */
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("Variable a: "+a);
+    }
+}
