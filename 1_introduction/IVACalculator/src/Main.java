b4f972af2902f706ff2a27bdd8c47007ae8d4b72
N0NamE1009 <luis.dbg1123@outlook.com>
2024-08-05 08:40:02 -0600

    Basic render  calculator program

--- 1_introduction/IVACalculator/src/Main.java
+++ 1_introduction/IVACalculator/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        float iva=16;
+        float iva=16                                ;
         System.out.println("Ingrese el precio del articulo: ");
         float precio = console.nextFloat();
         float totalAgregar =iva*precio/100;
---
+++ 1_introduction/RenderCalculator/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double precioPorMinuto=0.05f;
+        double cantMinutos=2322424;
+        double precioFinal=cantMinutos*precioPorMinuto;
+
+        System.out.println("Precio Final: $"+precioFinal);
+    }
+}
