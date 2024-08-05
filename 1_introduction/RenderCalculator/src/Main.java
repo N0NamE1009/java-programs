abee1e9e5a134916079196b4658e65f4e9f295c6
N0NamE1009 <luis.dbg1123@outlook.com>
2024-08-05 08:46:40 -0600

    Basic render  calculator program

--- 1_introduction/RenderCalculator/src/Main.java
+++ 1_introduction/RenderCalculator/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner console= new Scanner(System.in);
+        System.out.println("Indique la cantidad de minutos a grabar: ");
+
         double precioPorMinuto=0.05f;
-        double cantMinutos=2322424;
+        double cantMinutos=console.nextDouble();
         double precioFinal=cantMinutos*precioPorMinuto;
 
         System.out.println("Precio Final: $"+precioFinal);
