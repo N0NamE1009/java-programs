8183d70f813b478671c5a3e14d77b71d6940dde7
N0NamE1009 <luis.dbg1123@outlook.com>
2024-08-05 06:45:37 -0600

    Basic IVA  calculator program

---
+++ 1_introduction/IVACalculator/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        float iva=16;
+        System.out.println("Ingrese el precio del articulo: ");
+        float precio = console.nextFloat();
+        float totalAgregar =iva*precio/100;
+        float precioFinal=precio + totalAgregar;
+        System.out.println("Precio Total: "+precioFinal);
+
+    }
+}
